9e7992b077c347fbe7dc16b9c0c1731c2c46406b
UnaiiGonzalezz <unai.gonzalez@opendeusto.es>
2025-03-28 12:41:38 +0100

    Creación ClienteRepository.java

---
+++ src/main/java/com/example/restapi/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package main.java.com.example.restapi.repository;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.restapi.model.Cliente;
+ 
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+ 
+    // Buscar un cliente por su número de teléfono
+    Cliente findByTelefono(String telefono);
+ 
+ }
