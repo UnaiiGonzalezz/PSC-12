40f13f949a8192bb3f5f633bc19f2148533e71c2
UnaiiGonzalezz <unai.gonzalez@opendeusto.es>
2025-03-28 12:33:28 +0100

    Creacion MedicamentoRepository.java

---
+++ src/main/java/com/example/restapi/repository/MedicamentoRepository.java
@@ -0,0 +1,17 @@
+package main.java.com.example.restapi.repository;
+ 
+ import java.util.List;
+ 
+ import org.springframework.data.jpa.repository.JpaRepository;
+ import org.springframework.stereotype.Repository;
+ import com.example.restapi.model.Medicamento;
+
+import main.java.com.example.restapi.model.Medicamento;
+ 
+ @Repository
+ public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
+ 
+     // Método que obtiene los medicamentos que estan disponibles
+     List<Medicamento> findByDisponibleTrue();
+ 
+ }
