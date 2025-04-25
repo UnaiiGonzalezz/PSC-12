e87bebee5c897020a9259a29c61677e1b09af094
PabloCapelastegui <pablo.capelastegui@opendeusto.es>
2025-04-25 11:39:07 +0200

    Getters y setters ClienteDTO

--- src/main/java/com/example/restapi/model/dto/ClienteDTO.java
+++ src/main/java/com/example/restapi/model/dto/ClienteDTO.java
@@ -18,5 +18,45 @@ public class ClienteDTO {
         this.metodoPago = metodoPago;
     }
 
-    /* getters & setters … */
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
 }
