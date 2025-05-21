3f4a70a7024b35b4250472437eb961b1f11a19f1
UnaiiGonzalezz <unai.gonzalez@opendeusto.es>
2025-05-21 18:39:24 +0200

    Reestructuración CompraControllerExtraTest para correcto funcionamiento

--- src/test/java/com/example/restapi/controller/CompraControllerExtraTest.java
+++ src/test/java/com/example/restapi/controller/CompraControllerExtraTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,8 +22,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc(addFilters = false)   // desactiva filtros de Spring-Security
+@WebMvcTest(CompraController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @Tag("controller")
 class CompraControllerExtraTest {
 
